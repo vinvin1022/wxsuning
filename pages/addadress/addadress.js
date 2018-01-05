2b5993a30c91c57f9307cf970749dd069cbc10ec
vinvin1022 <vin_i@foxmail.com>
2018-01-05 08:48:40 +0800

    dizhi

--- pages/addadress/addadress.js
+++ pages/addadress/addadress.js
@@ -6,16 +6,16 @@ Page({
    */
   data: {
     sexs: [
-      { name: '0', value: '先生', checked: true},
+      { name: '0', value: '先生', checked: true },
       { name: '1', value: '女士', checked: false }
     ],
-    dianpu:"",
-    fanwei:"",
-    sendaddress:"",
-    bianhao:"",
-    chenghu:"",
-    phone:"",
-    sex:0
+    dianpu: "",
+    fanwei: "",
+    sendaddress: "",
+    bianhao: "",
+    chenghu: "",
+    phone: "",
+    sex: 0
 
 
   },
@@ -24,10 +24,10 @@ Page({
       sex: e.detail.value
     })
   },
-  dianpuinput(e){
+  dianpuinput(e) {
     console.log(e);
     this.setData({
-      dianpu:e.detail.value
+      dianpu: e.detail.value
     })
   },
   fanweiinput(e) {
@@ -61,8 +61,8 @@ Page({
     })
   },
 
-  save(){
-    let address={
+  save() {
+    let address = {
       dianpu: this.data.dianpu,
       fanwei: this.data.fanwei,
       sendaddress: this.data.sendaddress,
@@ -73,9 +73,9 @@ Page({
     }
     console.log(JSON.stringify(address))
     wx.setStorage({
-      key:"address",
+      key: "address",
       data: JSON.stringify(address),
-      success:()=>{
+      success: () => {
         wx.showToast({
           title: '保存成功',
           icon: 'success',
@@ -83,24 +83,29 @@ Page({
         })
       }
     })
-  
+
   },
-  submit(options){
+  submit(options) {
     console.log(options)
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    if (!wx.getStorageSync("address")){
+      return;
+    }
     let _this = this;
+
     let data = JSON.parse(wx.getStorageSync("address"));
-  
+
     console.log(data);
 
 
-    for (let k = 0; k < _this.data.sexs.length;k++){
+    for (let k = 0; k < _this.data.sexs.length; k++) {
       _this.data.sexs[k].checked = false;
-      if (data.sex == _this.data.sexs[k].name){
+      if (data.sex == _this.data.sexs[k].name) {
         _this.data.sexs[k].checked = true;
       }
     }
@@ -115,55 +120,55 @@ Page({
       sexs: _this.data.sexs
     })
 
-   
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- pages/balance/balance.js
+++ pages/balance/balance.js
@@ -5,10 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    product:[],
-    allprice:0,
-    discount:10,
-    expressfee:0,
+    product: [],
+    allprice: 0,
+    discount: 10,
+    expressfee: 0,
     address: ""
   },
 
@@ -16,7 +16,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let product = JSON.parse(options.carsproduct).filter((product)=>product.num>0);
+    let product = JSON.parse(options.carsproduct).filter((product) => product.num > 0);
     let address = JSON.parse(wx.getStorageSync("address"));
     console.log(address);
     this.setData({
@@ -30,13 +30,13 @@ Page({
   },
 
 
-  computedprice(){
-    return this.data.product.reduce((price,product)=>{
+  computedprice() {
+    return this.data.product.reduce((price, product) => {
       return price += parseFloat(product.price * product.num);
     }, (this.data.expressfee - this.data.discount))
   },
   //跳转到新增收货地址
-  goAddAddress(){
+  goAddAddress() {
     wx.navigateTo({
       url: '/pages/addadress/addadress',
     })
@@ -48,7 +48,7 @@ Page({
    */
   onReady: function (options) {
     console.log("onReady")
-  
+
   },
 
   /**
@@ -62,34 +62,34 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
